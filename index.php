92d6d6bda91461f5f96128deb1465c2f0996ad19
Whiteverse <vadheljamal@gmail.com>
2022-11-03 11:48:52 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptate, eos culpa corrupti omnis voluptatibus at. Animi ipsam maxime earum ullam? Atque ratione at quis sunt pariatur autem quia tempora.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
